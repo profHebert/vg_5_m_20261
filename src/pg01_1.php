b90424d1160118d506977c728cece34ddf638044
Hebert Bratefixe Alquimim <hebert@uni9.pro.br>
2026-03-12 14:34:04 +0000

    aula 120326

---
+++ src/pg01_1.php
@@ -0,0 +1,34 @@
+
+<?php
+echo "<h1>Tópico 02 </h1>";//escrever anotações
+echo "Outra linha<br>";#anotações
+//print "funciona com print<br>";
+/*
+anotar
+varias
+linhas
+*/
+echo phpversion();
+echo "<hr>";
+
+$idade=25;//int
+$nota=8.5;//float
+$nome="Beto ";//string
+$nota1=5.6;
+
+echo $nome;
+
+//Bete, você tem 25 anos e sua nota foi 8.5
+echo "<hr>";
+echo $nome;
+echo ", você tem ";
+echo $idade;
+echo " anos e a sua nota foi ";
+echo $nota;
+echo "<hr>";
+echo "$nome, você tem <b>$idade</b> anos e sua nota foi $nota";
+
+echo "<hr>";
+var_dump($nome);
+?>
+<!-- <p>Alguma coisa</p>  <b> </b> -->
